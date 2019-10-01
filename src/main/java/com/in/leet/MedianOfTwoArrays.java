eb763d85cd387631f59b38e26808b797eaaa9118
singh.kulwant@gmx.com <singh.kulwant@gmx.com>
2019-10-01 23:38:38 +0530

    1 Oct updated array median soln

--- src/main/java/com/in/leet/MedianOfTwoArrays.java
+++ src/main/java/com/in/leet/MedianOfTwoArrays.java
@@ -50,11 +50,12 @@ public class MedianOfTwoArrays {
 
 		for (int i = 0; i < Math.max(nums1.length, nums2.length); i++) {
 
-			if (i < nums1.length && nums1[i] != 0) {
+			if (i < nums1.length ) {
 				list.add(nums1[i]);
+				flag = false;
 			}
 
-			if (i < nums2.length && nums2[i] != 0) {
+			if (i < nums2.length ) {
 				list.add(nums2[i]);
 				flag = false;
 			}

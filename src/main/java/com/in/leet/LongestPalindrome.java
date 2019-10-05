553f60fe42d81986d100df14c3c917581d9c7c48
singh.kulwant@gmx.com <singh.kulwant@gmx.com>
2019-10-05 13:54:48 +0530

    5 Oct palindrome solution

---
+++ src/main/java/com/in/leet/LongestPalindrome.java
@@ -0,0 +1,42 @@
+package com.in.leet;
+
+public class LongestPalindrome {
+	public static void main(String[] args) {
+		System.out.println(longestPalindrome("abbadfaba"));
+	}
+
+	public static String longestPalindrome(String s) {
+		int len = s.length();
+
+		if (len == 0)
+			return "";
+
+		int max = 0;
+		String str = String.valueOf(s.charAt(0));
+
+		for (int i = 0; i < len; i++) {
+			for (int j = len - 1; j >= 0; j--) {
+				if (check(s, i, j)) {
+					if (j - i > max) {
+						max = j - i;
+						str = s.substring(i, j + 1);
+					}
+				}
+			}
+		}
+
+		return str;
+	}
+
+	private static boolean check(String s, int i, int j) {
+
+		for (; i <= j; i++) {
+			if (!(s.charAt(i) == s.charAt(j))) {
+				return false;
+			} else {
+				j--;
+			}
+		}
+		return true;
+	}
+}
---
+++ src/main/java/com/in/leet/PalindromeCheck.java
@@ -0,0 +1,21 @@
+package com.in.leet;
+
+public class PalindromeCheck {
+	public static void main(String[] args) {
+		String s = "abaaba";
+		System.out.println(check(s));
+	}
+
+	private static boolean check(String s) {
+		int j = s.length() - 1;
+		for (int i = 0; i <= j; i++) {
+			if (!(s.charAt(i) == s.charAt(j))) {
+				return false;
+			} else {
+				j--;
+			}
+		}
+		return true;
+	}
+
+}

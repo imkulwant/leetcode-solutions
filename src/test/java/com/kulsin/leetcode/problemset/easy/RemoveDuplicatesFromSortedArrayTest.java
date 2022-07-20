a1a108160e44fa865bbab4e75ec1cf84094d735d
singh-kulwant <singh.kulwant@gmx.com>
2022-07-21 00:49:53 +0200

    07-21: remove duplicates from sorted array

---
+++ src/test/java/com/kulsin/leetcode/problemset/easy/RemoveDuplicatesFromSortedArrayTest.java
@@ -0,0 +1,36 @@
+package com.kulsin.leetcode.problemset.easy;
+
+import org.junit.jupiter.api.Test;
+
+class RemoveDuplicatesFromSortedArrayTest {
+
+    @Test
+    void removeDuplicatesTestExampleOne() {
+
+        int[] input = new int[]{1, 1, 2};
+        int[] expectedArray = new int[]{1, 2};
+        int response = RemoveDuplicatesFromSortedArray.removeDuplicates(input);
+
+        assert response == 2;
+
+        for (int i = 0; i < response; i++) {
+            assert input[i] == expectedArray[i];
+        }
+    }
+
+    @Test
+    void removeDuplicatesTestExampleTwo() {
+
+        int[] input = new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        int[] expectedArray = new int[]{0, 1, 2, 3, 4};
+
+        int response = RemoveDuplicatesFromSortedArray.removeDuplicates(input);
+
+        assert response == 5;
+
+        for (int i = 0; i < response; i++) {
+            assert input[i] == expectedArray[i];
+        }
+    }
+
+}

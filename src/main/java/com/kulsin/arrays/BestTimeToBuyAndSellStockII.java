8eb38245d5f13f3a775704607350022a1d23063e
Kulwant Singh <kulwant.singh@leovegas.com>
2024-05-12 06:08:44 +0200

    122. Best Time to Buy and Sell Stock II

--- src/main/java/com/kulsin/arrays/BestTimeToBuyAndSellStockII.java
+++ src/main/java/com/kulsin/arrays/BestTimeToBuyAndSellStockII.java
@@ -8,22 +8,19 @@ package com.kulsin.arrays;
 public class BestTimeToBuyAndSellStockII {
 
     /**
-     * Runtime: 
-     * Memory: 
+     * Runtime: 1ms
+     * Memory: 45.38MB
      */
     public int maxProfit(int[] prices) {
-
-        int minPrice = prices[0];
         int maxProfit = 0;
 
-        for (int i = 1; i< prices.length ; i++) {
-            if(prices[i] - minPrice > maxProfit) {
-                maxProfit = prices[i] - minPrice;
-                minPrice = prices[i];
-                
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] - prices[i - 1] > 0) {
+                maxProfit += prices[i] - prices[i - 1];
             }
         }
 
+        return maxProfit;
     }
 
 }
